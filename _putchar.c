37951ea3f1887a2b266aa0c1db8e50f847d652ab
Salq24 <qogheneochuko@gmail.com>
2023-12-05 01:24:42 -0800

    printf

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - program that prints
+ * Return: 1 (success)
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
